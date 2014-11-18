849c8a0017fab5198aee9bf99ae4d6ba716ab9f4
parkjiyun <jyun38@gmail.com>
2014-11-18 19:32:38 +0900

    ddd

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var solar, bg, earth, moon;
+function setup() {
+    createCanvas(1000, 1000);
+	solar = loadImage("sun.gif");
+	bg = loadImage("bg.jpg");
+	earth = loadImage("earth.gif");
+	moon = loadImage("moon.gif");
+}
+
+
+function draw() {
+
+	background(bg);
+
+	var duration0 = 7000;
+	var timing0 = (new Date()%duration0)/duration0;
+	var duration1 = 6000;
+	var timing1 = (new Date()%duration1)/duration1;
+	var duration2 = 5000;
+	var timing2 = (new Date()%duration2)/duration2;
+	var duration3 = 4000;
+	var timing3 = (new Date()%duration3)/duration3;
+	var duration4 = 3000;
+	var timing4 = (new Date()%duration4)/duration4;
+
+	//지구라인
+	noFill();
+	stroke(255);
+	ellipse(500,500,600,600)
+
+	//달타원라인1
+	noFill();
+	stroke(255);
+	ellipse(500 + Math.cos(timing1*2*PI)*300,
+        500 + Math.sin(timing1*2*PI)*300,
+        100,
+        300);
+
+	//달타원라인2
+	noFill();
+	stroke(255);
+	ellipse(500 + Math.cos(timing1*2*PI)*300,
+        500 + Math.sin(timing1*2*PI)*300,
+        300,
+        150);
+
+	//달라인
+	noFill();
+	stroke(255);
+	ellipse(500 + Math.cos(timing1*2*PI)*300,
+        500 + Math.sin(timing1*2*PI)*300,
+        200,
+        200);
+
+	//태양
+ 	image(solar,250,250,500,500);
+
+
+ 	//달1
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*150-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*70-100,200,200);
+
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing3*4*PI)*100-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing3*4*PI)*100-100,200,200);
+
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing4*4*PI)*50-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing4*4*PI)*150-100,200,200);
+
+ 	//지구
+	image(earth,500 + Math.cos(timing1*2*PI)*300-150,500 + Math.sin(timing1*2*PI)*300-150,300,300);
+}
